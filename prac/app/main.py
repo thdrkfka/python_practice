a1aa76222b8a65be407b30e124223fc9392fa2cd
DESKTOP-0L11UB0\user <ssongjiaran@gmail.com>
2024-06-14 12:00:22 +0900

    update : [송가람] MySQL 연동 성공 세부사항 - DB에 연동하여 입력값 받아서 저장되는지 확인

--- prac/app/main.py
+++ prac/app/main.py
@@ -2,8 +2,9 @@
 #  2. 기존 db와 매핑된 models.py를 가져와서
 #  3. query를 이용해 filter를 설정해서 db 값을 가져오기
 
-from fastapi import FastAPI, Depends, Request
+from fastapi import *
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from .database import engine, get_db
 from .models import Test
@@ -19,3 +20,11 @@ def on_startup():
 async def read_root(request: Request, db: Session = Depends(get_db)):
     items = db.query(Test).all()
     return templates.TemplateResponse("index.html", {"request": request, "items": items})
+
+
+@app.post("/add")
+async def add_item(name: str = Form(...), number: int = Form(...), db: Session = Depends(get_db)):
+    new_item = Test(name=name, number=number)
+    db.add(new_item)
+    db.commit()
+    return RedirectResponse(url="/", status_code=303)
